ba572f07771dfb4b7a0877c10e3335a276a2eade
Tom <tsanchis@edenschool.fr>
2022-04-20 00:27:11 +0200

    ScrollSpy finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const backprogress = document.querySelector(".barprogress");
 
 window.addEventListener("scroll", () => {
-  backprogress.style.width = `${scrollY}%`;
+  const scroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  backprogress.style.width = `${scroll / height * 100}%`;
 })
