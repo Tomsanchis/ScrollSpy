30c9db993b65573536e3903da6bce392b4b435fe
Tom <tsanchis@edenschool.fr>
2022-05-21 13:47:52 +0200

    update variable scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const backprogress = document.querySelector(".barprogress");
 
 window.addEventListener("scroll", () => {
-  const scroll = document.body.scrollTop || document.documentElement.scrollTop;
-  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  backprogress.style.width = `${scroll / height * 100}%`;
-})
+  const scroll = document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  backprogress.style.width = `${(scroll / height) * 100}%`;
+});
